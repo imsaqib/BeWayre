d2ef53ba85e99e42f9c1f0fc6a2589acc820fa68
Md Saqib <59443540+imsaqib@users.noreply.github.com>
2020-12-02 10:35:29 +0530

    Update CodeMB.py

--- CodeMB.py
+++ CodeMB.py
@@ -139,15 +139,11 @@ def intersect(circle_center, circle_radius, pt1, pt2, full_line=True):
 
 
 def geo(address):
-    api_key = False
-    if api_key is False:
-        api_key = 42
-        serviceurl = 'http://py4e-data.dr-chuck.net/json?'
-    else:
-        serviceurl = 'https://maps.googleapis.com/maps/api/directions/outputFormat?parameters'
+    api_key = 42
+    serviceurl = 'http://py4e-data.dr-chuck.net/json?'
     parms = dict()
     parms['address'] = address
-    if api_key is not False: parms['key'] = api_key
+    parms['key'] = api_key
     url = serviceurl + urllib.parse.urlencode(parms)
     uh = urllib.request.urlopen(url)
     data = uh.read().decode()
